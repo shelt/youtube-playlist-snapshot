8b9ce91f2333518dac04438d5c3f21eba3c29da0
Sam Shelton <sam@shelt.ca>
2015-05-13 17:09:20 -0500

    Support very large playlists

--- snapshot.py
+++ snapshot.py
@@ -1,52 +1,54 @@
-import os
+import os,sys
 import requests
 from xml.etree import ElementTree as etree
 
 AP = "{http://www.w3.org/2005/Atom}" # Atom prefix
 
-def process(id):
-    path = "https://gdata.youtube.com/feeds/api/playlists/" + id
+def snapshot(ID):
+    path = "https://gdata.youtube.com/feeds/api/playlists/" + ID
     response = requests.get(path)
     
     if response.text == "Playlist not found":
-        sys.exit("Playlist not found")
+        sys.exit("Error: Playlist not found")
     elif response.text == "User authentication required.":
-        sys.exit("Playlist is private. API Auth to come!")
+        sys.exit("Error: Playlist is private. API Auth to come!")
     
-    # Parsing
-    root = etree.fromstring(response.text)
-    print("Processing playlist: " + root.find(AP+"title").text)
-    
+    # GET CURRENT VIDEOS
     cur_videos = {}
-    for entry in root.findall(AP+"entry"):
-        link  = entry.findall(AP+"link")[2].attrib['href'] # todo
-        title = entry.find(AP+"title").text
-        cur_videos[link] = title
-    
+    startindex = 1
+    while True:
+        response = requests.get(path + "?start-index=" + str(startindex))
+        root = etree.fromstring(response.text)
+        entries = root.findall(AP+"entry")
+        if len(entries) == 0:
+            break
+
+        for entry in entries:
+            id    = entry.find(AP+"id").text
+            title = entry.find(AP+"title").text
+            cur_videos[id] = title
+        startindex += 25
+        
     # CREATE/PARSE DATAFILE
-    
     if not os.path.isfile("plists.xml"): # Datafile doesn't exist
         playlists = etree.Element("playlists")
     else:
-        # PARSE DATAFILE
         playlists = etree.parse("plists.xml").getroot()
     
     # GET/CREATE PLAYLIST
-    plist = playlists.find(id)
-    if not plist: # Playlist not present
-        plist = etree.SubElement(playlists, id)
-        
+    plist = playlists.find(ID)
+    if plist == None: # Playlist not present
+        plist = etree.SubElement(playlists, ID)
         
     # CHECK FOR DELETED VIDEOS
     for video in plist:
-        if video.attrib["link"] not in cur_videos:
+        if video.attrib["id"] not in cur_videos:
             video.attrib["deleted"] = "true"
             print("DELETED: " + video.text)
     # ADD NEW VIDEOS
-    for link,title in cur_videos.items():
-        if not plist.find("video[@link='"+link+"']"): # video not already present       NOT WORKING - TODO
-            print("HSH")
-            etree.SubElement(plist, "video", attrib={"link":link, "deleted":"false"}).text = title
+    for id,title in cur_videos.items():
+        if plist.find("video[@id='"+id+"']") == None: # video not already present
+            etree.SubElement(plist, "video", attrib={"id":id, "deleted":"false"}).text = title
         
     # WRITE DATA TO FILE
     tree = etree.ElementTree(playlists)
@@ -58,10 +60,18 @@ def process(id):
         
         
 def main():
-    print("Playlist ID: ",)
-    #id = input()
-    id = "PLeVHlcjtKzgSFDnvbqvuPJI8cW_xWZQd1"
-    
-    process(id)
+    ids = input("Playlist ID(s): ")
+    if ids == "":
+        try:
+            for plist in etree.parse("plists.xml").getroot().getchildren():
+                snapshot(plist.tag)
+                
+        except TodoError:
+            sys.exit("Error: No playlist IDs entered and no history found.")
+            
+    else:
+        for id in ids.split():
+            snapshot(id)
+    print("Completed.")
     
 main()
